5694d0c83b024c782022b099c53f0e50c2dae745
Rishikesh Suvarna <rishikeshsuvarna@gmail.com>
2024-10-28 01:40:41 +0000

    refactor: Update trust proxy setting in express app configuration

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 // Trust the reverse proxy
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 
 // Middleware to parse incoming request data
 app.use(express.json());
